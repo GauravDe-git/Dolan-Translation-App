cb3d851401b8c33125cbbb1c1908f0e1938b38a5
Gaurav De <gm.gauravde@gmail.com>
2021-09-07 15:34:16 +0530

    created files index, styles and app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var translatebutton = document.querySelector("#btn-translate");
+var textinput = document.querySelector("#txt-input");
+var outputtext = document.querySelector("#output");
+var url = "https://api.funtranslations.com/translate/dolan.json";
+
+function urlEdit(input) {
+    return url + "?" + "text=" + input;
+}
+
+translatebutton.addEventListener("click", () => {
+    var response = (one) => one.json();
+    var display = (two) => {
+        var returned = two.contents.translated;
+        outputtext.innerText = returned;
+    };
+    fetch(urlEdit(textinput.value))
+        .then(response)
+        .then(display)
+        .catch((error) => {
+            alert(
+                "you have used the app for the fifth time, come after an hour."
+            );
+            return console.log("error came!!", error);
+        });
+});
